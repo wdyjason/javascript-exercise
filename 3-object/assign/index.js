7fd74499b41c9995c1b4136e32ca53a2a5949dac
wdyjason <ddd123321456555@qq.com>
2020-08-10 16:28:55 +0800

    [wdyjason] feat: finish obejce assign test

--- 3-object/assign/index.js
+++ 3-object/assign/index.js
@@ -1,3 +1,12 @@
 export default function addSerialNumber(source) {
   // TODO 5: 在这里写实现代码，需要采用ES6 Object.assign
+  const target = {
+    serialNumber: '12345',
+    properties: {
+      color: 'green',
+      status: 'processed'
+    }
+  };
+  const result = Object.assign(source, target);
+  return result;
 }
