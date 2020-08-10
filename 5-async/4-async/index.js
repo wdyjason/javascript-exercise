e932d8bf73875763e8b0b16dcf48e4a13b07edf7
wdyjason <ddd123321456555@qq.com>
2020-08-10 21:25:03 +0800

    [wdyjason] feat: finish async/await test

--- 5-async/4-async/index.js
+++ 5-async/4-async/index.js
@@ -1,6 +1,15 @@
 async function fetchData(url) {
   // <-- start
   // TODO 24: 通过await/async实现异步请求
+  const result = await fetch(url)
+    .then(response => {
+      if (response.status === 200) {
+        return response.json();
+      }
+      throw new Error('error');
+    })
+    .catch(error => console.error(error));
+  document.writeln(result.name);
   // end -->
 }
 
